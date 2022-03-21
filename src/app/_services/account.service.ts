2f94cd0ff29bf309f448b7b1f92cd4a41d778b19
Usman Tahir <Usman.tahir@synergy-it.com>
2022-03-21 23:47:12 +0500

    add navbar & service

---
+++ src/app/_services/account.service.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+baseUrl='http://localhost:23470/api/';
+  constructor(private http:HttpClient) { }
+
+  login(model:any){
+    return this.http.post(this.baseUrl,model);
+  }
+}
