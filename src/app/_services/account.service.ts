97b89712250efb96a19d7d25dd62d7d2fee46813
Usman Tahir <Usman.tahir@synergy-it.com>
2022-03-23 17:53:39 +0500

    add persistance in login & register form

--- src/app/_services/account.service.ts
+++ src/app/_services/account.service.ts
@@ -1,14 +1,39 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ReplaySubject } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { User } from '../_models/User';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
 baseUrl='http://localhost:23470/api/';
+private currentUserSource=new ReplaySubject<User>(1);
+currentUser$=this.currentUserSource.asObservable();
   constructor(private http:HttpClient) { }
 
   login(model:any){
-    return this.http.post(this.baseUrl+'account/login',model);
+    return this.http.post(this.baseUrl+'account/login',model).pipe(
+      map((response:any)=>{
+        if(response && response.isSuccess){
+          const user:User=response.result;
+          localStorage.setItem('user',JSON.stringify(user));
+          this.currentUserSource.next(user);
+        }
+      })
+    );
+  }
+
+  setCurrentUser(user:User){
+    this.currentUserSource.next(user);
+  }
+
+  logout(){
+    localStorage.removeItem('user');
+    this.currentUserSource.next(null);
   }
 }
+
+
+
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from './_services/account.service';
 
 @Component({
   selector: 'app-root',
@@ -11,13 +12,18 @@ export class AppComponent implements OnInit {
   title = 'The Dating App';
   users:any;
 
-  constructor(private http: HttpClient){
+  constructor(private http: HttpClient, private accountService: AccountService){
 
   }
   ngOnInit(): void {
     this.getUsers();
+    this.setCurrentUser();
   }
 
+  setCurrentUser(){
+    const user=JSON.parse(localStorage.getItem('user'));
+    this.accountService.setCurrentUser(user);
+  }
   getUsers(){
     this.http.get('http://localhost:23470/api/Users').subscribe(response=>{
     this.users=response;
