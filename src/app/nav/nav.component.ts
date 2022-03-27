a5904a11f2891968b77cb09e3313b90798a1ce82
Usman Tahir <Usman.tahir@synergy-it.com>
2022-03-27 16:41:30 +0500

    Add toastr service

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -9,7 +10,7 @@ import { AccountService } from '../_services/account.service';
 })
 export class NavComponent implements OnInit {
 model:any={};
-  constructor(public accountService: AccountService,private router:Router) { }
+  constructor(public accountService: AccountService,private router:Router, private toastr: ToastrService) { }
 
   ngOnInit(): void {
   }
@@ -19,6 +20,7 @@ model:any={};
       this.router.navigateByUrl('/members');
     },error=>{
       console.log(error);
+      this.toastr.error(error.error.message);
     })
   }
 
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -11,16 +12,18 @@ export class RegisterComponent implements OnInit {
 model:any={};
 // @Input() UserFromHomeComponent:any;
 @Output() cancelRegister=new EventEmitter();
-  constructor(private accountService:AccountService) { }
+  constructor(private accountService:AccountService, private toastr: ToastrService) { }
 
   ngOnInit(): void {
   }
   register(){
     this.accountService.register(this.model).subscribe((response)=>{
       console.log(response);
-      this.cancel()
+      this.cancel();
     },error=>{
+      debugger;
       console.log(error);
+      this.toastr.error(error.error.message);
     })
   }
 
