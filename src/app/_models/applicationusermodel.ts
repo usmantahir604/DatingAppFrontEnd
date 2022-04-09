2b8dd1274d2f3608d71857a060a137753cd7db04
Usman Tahir <Usman.tahir@synergy-it.com>
2022-04-10 01:01:18 +0500

    Create ApplicationUserModel or Member

---
+++ src/app/_models/applicationusermodel.ts
@@ -0,0 +1,18 @@
+import { PhotoModel } from "./photo";
+
+export interface ApplicationUserModel {
+    id: string;
+    username: string;
+    photoUrl: string;
+    age: number;
+    knownAs: string;
+    created: Date;
+    lastActive: Date;
+    gender: string;
+    introduction: string;
+    lookingFor: string;
+    interests: string;
+    city: string;
+    country: string;
+    photos: PhotoModel[];
+}
--- src/app/_services/account.service.ts
+++ src/app/_services/account.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ReplaySubject } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { User } from '../_models/User';
+import { User } from '../_models/user';
 
 @Injectable({
   providedIn: 'root'
