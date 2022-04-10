62e9fd886096b216a1603848d1451e1119fada1a
Usman Tahir <Usman.tahir@synergy-it.com>
2022-04-11 00:02:58 +0500

    Implement jwt interceptor

--- src/app/_services/members.service.ts
+++ src/app/_services/members.service.ts
@@ -3,11 +3,7 @@ import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { ApplicationUserModel } from '../_models/applicationusermodel';
 
-const httpOptions={
-  headers:new HttpHeaders({
-    Authorization:'Bearer '+JSON.parse(localStorage.getItem('user')).token
-  })
-};
+
 @Injectable({
   providedIn: 'root'
 })
@@ -16,10 +12,10 @@ export class MembersService {
   constructor(private http:HttpClient) { }
 
   getMembers(){
-    return this.http.get<ApplicationUserModel[]>(this.baseUrl+'Users',httpOptions)
+    return this.http.get<ApplicationUserModel[]>(this.baseUrl+'Users')
   }
 
   getMember(username:string){
-    return this.http.get<ApplicationUserModel>(this.baseUrl+'Users/GetUser'+username,httpOptions);
+    return this.http.get<ApplicationUserModel>(this.baseUrl+'Users/GetUser'+username);
   }
 }
