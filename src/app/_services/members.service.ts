9d764aef3b7f8fc5eac4563d567ed8ed66760c4a
Usman Tahir <Usman.tahir@synergy-it.com>
2022-04-16 16:35:15 +0500

    loading indicator & using service to store state

--- src/app/_services/members.service.ts
+++ src/app/_services/members.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { of } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { ApplicationUserModel } from '../_models/applicationusermodel';
 
@@ -9,17 +11,28 @@ import { ApplicationUserModel } from '../_models/applicationusermodel';
 })
 export class MembersService {
   baseUrl=environment.apiUrl;
+  members:ApplicationUserModel[];
   constructor(private http:HttpClient) { }
 
   getMembers(){
+    if(this.members?.length>0) return of(this.members);
     return this.http.get<ApplicationUserModel[]>(this.baseUrl+'Users')
+    .pipe(map(members=>{
+      this.members=members;
+      return members;
+    }));
   }
 
   getMember(username:string){
+    const member=this.members.find(x=>x.username==username);
+    if(member!==undefined) return of(member);
     return this.http.get<ApplicationUserModel>(this.baseUrl+'Users/'+username);
   }
 
   updateMember(member:ApplicationUserModel){
-    return this.http.put(this.baseUrl+'Users',member);
+    return this.http.put(this.baseUrl+'Users',member).pipe(map(()=>{
+      const index=this.members.indexOf(member);
+      this.members[index]=member;
+    }));
   }
 }
