3df259d1555c44c8073077905e96d10615601962
Usman Tahir <Usman.tahir@synergy-it.com>
2022-04-23 16:41:48 +0500

    Add reactive form and validation

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
@@ -10,21 +11,42 @@ import { AccountService } from '../_services/account.service';
 })
 export class RegisterComponent implements OnInit {
 model:any={};
+registerForm: FormGroup;
 // @Input() UserFromHomeComponent:any;
 @Output() cancelRegister=new EventEmitter();
   constructor(private accountService:AccountService, private toastr: ToastrService) { }
 
   ngOnInit(): void {
+    this.intializeForm();
   }
+
+  intializeForm(){
+    this.registerForm=new FormGroup({
+      userName:new FormControl('',Validators.required),
+      password: new FormControl('',[Validators.required,Validators.minLength(6),Validators.maxLength(8)]),
+      confirmPassword: new FormControl('',[Validators.required,this.matchValues('password')])
+    });
+    this.registerForm.controls.password.valueChanges.subscribe(()=>{
+      this.registerForm.controls.confirmPassword.updateValueAndValidity();
+    });
+  }
+
+  matchValues(matchTo): ValidatorFn {
+    return (control:AbstractControl)=>{
+      return control?.value===control?.parent?.controls[matchTo].value
+      ? null : {isMatching:true};
+    }
+  }
+
   register(){
-    this.accountService.register(this.model).subscribe((response)=>{
-      console.log(response);
-      this.cancel();
-    },error=>{
-      debugger;
-      console.log(error);
-      this.toastr.error(error.error.message);
-    })
+    console.log(this.registerForm.value);
+    // this.accountService.register(this.model).subscribe((response)=>{
+    //   console.log(response);
+    //   this.cancel();
+    // },error=>{
+    //   console.log(error);
+    //   this.toastr.error(error.error.message);
+    // })
   }
 
   cancel(){
