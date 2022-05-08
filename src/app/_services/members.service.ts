77f54d00b544822fabd8f20cda8607352c1e4644
Usman Tahir <Usman.tahir@synergy-it.com>
2022-05-08 16:53:31 +0500

    Restoring caching for member detailed Remembering filters for user

--- src/app/_services/members.service.ts
+++ src/app/_services/members.service.ts
@@ -1,11 +1,13 @@
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { of } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, take } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { ApplicationUserModel } from '../_models/applicationusermodel';
 import { PaginatedResult } from '../_models/pagination';
+import { User } from '../_models/user';
 import { UserParams } from '../_models/userParams';
+import { AccountService } from './account.service';
 
 
 @Injectable({
@@ -15,9 +17,29 @@ export class MembersService {
   baseUrl=environment.apiUrl;
   members:ApplicationUserModel[];
   memberCache=new Map();
-  
-  constructor(private http:HttpClient) { }
+  userParams:UserParams;
+  user:User;
 
+  constructor(private http:HttpClient, private accountService: AccountService) { 
+
+    this.accountService.currentUser$.pipe(take(1)).subscribe((user)=>{
+      this.user=user;
+      this.userParams=new UserParams(this.user);
+    })
+    
+  }
+
+  getUserParams(){
+    return this.userParams;
+  }
+
+  setUserParams(params:UserParams ){
+     this.userParams=params;
+  }
+  resetUserParams(){
+    this.userParams=new UserParams(this.user);
+    return this.userParams;
+  }
   getMembers(userParams:UserParams){
     var response=this.memberCache.get(Object.values(userParams).join('-'));
     if(response){
@@ -54,8 +76,13 @@ export class MembersService {
   }
 
   getMember(username:string){
-    const member=this.members?.find(x=>x.username==username);
-    if(member!==undefined) return of(member);
+    const member=[...this.memberCache.values()]
+      .reduce((arr,elem)=>arr.concat(elem.result),[])
+      .find((member:ApplicationUserModel)=>member.username===username);
+      if(member)
+      {
+        return of(member);
+      }
     return this.http.get<ApplicationUserModel>(this.baseUrl+'Users/'+username);
   }
 
--- src/app/members/member-list/member-list.component.ts
+++ src/app/members/member-list/member-list.component.ts
@@ -20,18 +20,16 @@ userParams:UserParams;
 user:User;
 genderList = [{ value: 'male', display: 'Males' }, { value: 'female', display: 'Females' }];
 
-  constructor(private memberService: MembersService, private accountService:AccountService) 
+  constructor(private memberService: MembersService) 
   { 
-    this.accountService.currentUser$.pipe(take(1)).subscribe((user)=>{
-      this.user=user;
-      this.userParams=new UserParams(this.user);
-    })
+    this.userParams=this.memberService.getUserParams();
   }
 
   ngOnInit(): void {
     this.loadMembers();
   }
   loadMembers(){
+    this.memberService.setUserParams(this.userParams);
     this.memberService.getMembers(this.userParams).subscribe((response)=>{
       this.members=response.result;
       this.pagination=response.pagination;
@@ -40,11 +38,12 @@ genderList = [{ value: 'male', display: 'Males' }, { value: 'female', display: '
 
   pageChanged(event:any){
     this.userParams.pageNumber=event.page;
+    this.memberService.setUserParams(this.userParams);
     this.loadMembers();
   }
 
   resetFilters(){
-    this.userParams=new UserParams(this.user);
+    this.userParams=this.memberService.resetUserParams();
     this.loadMembers();
   }
 }
