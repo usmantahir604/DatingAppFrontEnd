2e01b6db1f7ce3ae755c33dd9c486f41f4c3ae8b
Usman Tahir <Usman.tahir@synergy-it.com>
2022-05-21 17:49:49 +0500

    Sending messages and Fixing the photo weirdness

--- src/app/_services/message.service.ts
+++ src/app/_services/message.service.ts
@@ -11,13 +11,17 @@ export class MessageService {
   baseUrl=environment.apiUrl;
   constructor(private http:HttpClient) { }
 
-  getMessages(pageNumber,pageSize,conatiner){
-    let params=getPaginationHeaders(pageNumber,pageSize)
-    params=params.append('container',conatiner);
-    return getPaginatedResult<[Message]>(this.baseUrl + 'messages',pageNumber,this.http);
-      }
+  getMessages(pageNumber, pageSize, container) {
+    let params = getPaginationHeaders(pageNumber, pageSize);
+    params = params.append('container', container);
+    return getPaginatedResult<Message[]>(this.baseUrl + 'messages', params, this.http);
+  }
 
   getMessageThread(username:string){
     return this.http.get<Message[]>(this.baseUrl+'messages/thread/'+username);
+  }
+
+  sendMessage(username:string, content: string){
+    return this.http.post<Message>(this.baseUrl+'messages/',{recipientUsername:username, content});
   }
 }
--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -8,29 +8,44 @@ import { MessageService } from '../_services/message.service';
   styleUrls: ['./messages.component.css']
 })
 export class MessagesComponent implements OnInit {
-  messages:Message[]=[];
-  pagination:Pagination;
-  container='Unread';
-  pageNumber=1;
-  pageSize=5;
+  messages: Message[] = [];
+  pagination: Pagination;
+  container = 'Unread';
+  pageNumber = 1;
+  pageSize = 5;
+  loading = false;
+
   constructor(private messageService: MessageService) { }
 
   ngOnInit(): void {
     this.loadMessages();
   }
-  loadMessages(){
-    this.messageService.getMessages(this.pageNumber,this.pageSize,this.container).subscribe((reponse)=>{
-      this.messages=reponse.result;
-      this.pagination = reponse.pagination;
+
+  loadMessages() {
+    this.loading = true;
+    this.messageService.getMessages(this.pageNumber, this.pageSize, this.container).subscribe(response => {
+      this.messages = response.result;
+      this.pagination = response.pagination;
+      this.loading = false;
     })
   }
 
-  pageChanged(event:any){
-    if(this.pageNumber!==event.page){
-      this.pageNumber=event.page;
+  // deleteMessage(id: number) {
+  //   this.confirmService.confirm('Confirm delete message', 'This cannot be undone').subscribe(result => {
+  //     if (result) {
+  //       this.messageService.deleteMessage(id).subscribe(() => {
+  //         this.messages.splice(this.messages.findIndex(m => m.id === id), 1);
+  //       })
+  //     }
+  //   })
+
+  // }
+
+  pageChanged(event: any) {
+    if (this.pageNumber !== event.page) {
+      this.pageNumber = event.page;
       this.loadMessages();
     }
-    
   }
 
 }
